3eac66fb8fe405aae7dd830258f053b8a4dfb98e
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-04-19 11:38:18 +0500

    Chap 21-25====> till Q10 done

--- js/chap-21-25_string_methods.js
+++ js/chap-21-25_string_methods.js
@@ -88,84 +88,49 @@ function ques5() {
     // Write a program to find the character at 3rd index in the word “Pakistani” and display the result in your browser.
     
     // declare
-    var fruits = [];
+    var  string = "Pakistani";
 
-    // Initialize table num
-    fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+    var char_at_index = string.charAt(3); 
 
-    document.write("<h4>Print Array Item using Loop: </h4>");
+     document.write("<h4>Extract Character:");
+     document.write("  Using charAt(n):</h4>");
 
-    // Display array items 
-    for (var a = 0; a < fruits.length; a++) {
-        document.write(fruits[a] + "<br>");
-    }
-    document.write("<br><br>");
-    for (var b = 0; b < fruits.length; b++) {
-        document.write("Elements at index " + b + " is " + fruits[b] + ".<br>");
-    }
+     document.write("<b>String: " + string + "</b><br>");
+     document.write("<b>Character at index 3: " + char_at_index );
 }
 
 function ques6() {
-    // Repeat Q1 using string concat() method.
-    
-    // declare
-    var even, odd;
+    // Write a program that takes two user inputs for first and last name 
+    // using prompt and merge them in a new variable titled fullName. 
+    // Greet the user using his full name.
 
+    let first_name,
+        last_name,
+        fullName;
 
-    counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    first_name = prompt("Enter Your First Name:");
+    last_name = prompt("Enter Your Last Name:");
 
+    if (first_name != null) {
 
+        if (last_name != null) {
 
-    document.write("<h4>Generate Series:</h4>");
+            fullName = first_name.concat(" ",last_name);
+            console.log(fullName);
+            if (+first_name <= 0 || +first_name > 0 || +last_name <= 0 || +last_name > 0) {
+                alert("Invalid Input in First Name or Last Name.")
+            }
+            else {
+                alert("Hello " + fullName + "!\nWelcome in Web Development Portal")
+            }
 
-    // Display Counting 
-    document.write("<h4>Counting: </h4>");
-    for (var a = 1; a <= 15; a++) {
-        document.write(a + ",");
-    }
-    document.write("<br><br>");
-
-    // Display Reverse Counting 
-    document.write("<h4>Reverse Counting: </h4>");
-    for (var b = 10; b > 0; b--) {
-        document.write(b + ",");
-    }
-    document.write("<br><br>");
-
-    // Display Even 
-    document.write("<h4>Even: </h4>");
-
-    for (var b = 0; b <= 20; b++) {
-        if ((b % 2) == 0) {
-            even = b;
-            console.log(even);
-            document.write(even + ",");
         }
-
-    }
-
-    document.write("<br><br>");
-
-    // Display Odd 
-    document.write("<h4>Odd: </h4>");
-    for (var b = 0; b <= 20; b++) {
-        if ((b % 2) !== 0) {
-            odd = b;
-            console.log(odd);
-            document.write(odd + ",");
+        else {
+            alert("You didn't enter Last Name.\nSession Terminated.");
         }
-
     }
-
-    // Display Series 
-    document.write("<h4>Series: </h4>");
-    for (var b = 2; b <= 20; b += 2) {
-        if ((b % 2) == 0) {
-            even = b;
-            console.log(even);
-            document.write(even + "k,");
-        }
-
+    else {
+        alert("You didn't enter First Name.\nSession Terminated.");
     }
 }
 
@@ -173,108 +138,62 @@ function ques7() {
     // Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser. 
 
     // declare
-    var bakert_items,
-        search_input,
-        pos_search_input;
+    let  city = "Hydrabad",
+    replace = "Islam";
 
+document.write("<h4>City Name Replace:</h4>");
+document.write("City: " + city);
 
-    bakert_items = ["cake", "apple pie", "cookie", "chips", "patties"];
-
-
-    search_input = prompt("Welcome to My Bakery. What do you want to order sir/ma'am?\ncake , apple pie ,  cookie ,  chips ,  patties")
-    search_input = search_input.toLowerCase();
-    if (search_input != null) {
-        // Index of match Item
-        pos_search_input = bakert_items.indexOf(search_input);
-        // index of match in array
-        // if match posi... is >= 0
-        // if not match posi... is = -1
-        console.log(pos_search_input);
-        if (pos_search_input >= 0) {
-            alert(search_input + " is avaliable at index " + pos_search_input + " in our Bakery.");
-            document.write(search_input + " is <b>avaliable</b> at index " + pos_search_input + " in our Bakery.");
-        }
-        // if not match posi... is = -1
-        else {
-            alert("We are sorry. " + search_input + " is not avaliable in our Bakery.");
-            document.write("We are sorry. " + search_input + " is <b>not avaliable</b> in our Bakery.");
-        }
-    }
-    else {
-        alert("You Quit session.")
-    }
-
+city = city.replace("Hydr",replace);
+document.write("<br>After replacement: " + city);
 }
 
 function ques8() {
     // /Write a program to replace all occurrences of “and” in the string with “&” and display the result in your browser. var message = “Ali and Sami are best friends. They play cricket and football together.”;
    
-   
-    // declare
-    var num, large_num = 0;
+    let message = "Ali and Sami are best friends. They play cricket and football together.";
 
+    document.write("<h4>Replace elements Using RegEx:</h4>");
+    document.write("message: " + message);
 
-    num = [24, 53, 78, 91, 12];
-
-    for (var a = 0; a < num.length; a++) {
-
-        if (large_num < num[a]) {
-            console.log(num[a]);
-            large_num = num[a];
-            console.log("num[a]", num[a]);
-
-        }
-
-
-    }
-    console.log("large_num", large_num);
-    document.write("<h4>Largest Number in Array:</h4><br>");
-    document.write("Array items: 24 , 53 , 78 , 91 , 12 <br><br>");
-    document.write("The Largest Number is " + large_num);
-
+    message = message.replace(/and/gi,"&");
+    document.write("<br><br>After replacement: " + message);
 }
 
 function ques9() {
 
     // /Write a program that converts a string “472” to a number 472. Display the values & types in your browser.
+    let num = "472";
 
-    // declare
-    var num, small_num;
+    document.write("<h4>String into Number:</h4>");
+    document.write("Value: " , num);
+    document.write("<br>Type: " , typeof num);
 
-
-    num = [24, 53, 78, 91, 12];
-
-    for (var a = 1; a < num.length; a++) {
-
-        if (num[0] > num[a]) {
-            console.log(num[a]);
-            small_num = num[a];
-            console.log("num[a]", num[a]);
-
-        }
-
-
-    }
-    console.log("small_num", small_num);
-    document.write("<h4>Smallest Number in Array:</h4><br>");
-    document.write("Array items: 24 , 53 , 78 , 91 , 12 <br><br>");
-    document.write("The smallest Number is " + small_num);
+    num = parseInt(num , 10);
+    document.write("<br><br>Value: " , num);
+    document.write("<br>Type: " , typeof num);
 }
 
 function ques10() {
     // Write a program that takes user input. Convert and show the input in capital letters.
     
-    // declare
-    var number_for_multi = 5,
-        result = 1;
+    let in_string;
 
-    console.log("Print Multiples of 5 Ranging 1 to 100:");
-    document.write("<h4>Print Multiples of 5 Ranging 1 to 100:</h4>");
-    for (var a = 1; result < 100; a++) {
-        console.log(a);
-        result = a * number_for_multi;
-        document.write(result + ",");
+    in_string = prompt("Enter Any Content to Convert into Upper Case:");
+    
+    if (in_string != null && in_string != "")
+    {
+        document.write("<h4>Convert into Upper Case:</h4>");
+        document.write("User Input: " + in_string + "<br>");
+
+        var uppercase_str = in_string.toUpperCase();
+        document.write("Upper Case: " + uppercase_str);
     }
+    else
+    {
+        alert("You Quit Session.");
+    }
+       
 }
 
 function ques11() {
